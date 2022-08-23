705a9ac16bb9fabfe7823b82843b04c3f57e3a5d
Xifre Font <xifre.font.97@gmail.com>
2022-08-23 22:32:49 +0200

    Now classes with numbers are displayed

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,24 +2,40 @@ import { useEffect, useState } from "react";
 import useStarWars from "../../hooks/useStarWars";
 
 const App = (): JSX.Element => {
-  const { getStarshipCount } = useStarWars();
-  const [shipData, setShipData] = useState({
-    count: "0",
-  });
+  const { getStarshipCount, getStarships } = useStarWars();
+  const [ships, setShips] = useState("0");
+  const [shipList, setShipList] = useState({});
 
   useEffect(() => {
     (async () => {
       const ships = await getStarshipCount();
       if (ships) {
-        setShipData({ ...shipData, count: ships });
+        setShips(ships);
       }
     })();
-  }, [getStarshipCount, shipData]);
+  }, [getStarshipCount]);
+
+  useEffect(() => {
+    (async () => {
+      setShipList(await getStarships());
+    })();
+  }, [getStarships]);
 
   return (
     <div className="app">
-      <h2 className="header-title">Starhips</h2>
-      <span>Total ships: {shipData.count}</span>
+      <h1 className="header-title">Starwars test</h1>
+      <h2>Starhips</h2>
+      <span>Total ships: {ships}</span>
+      <h2>Starhips by class</h2>
+      <ul>
+        {Object.entries(shipList).map((ship) => (
+          <li>
+            <>
+              {ship[0].charAt(0).toUpperCase() + ship[0].slice(1)} ({ship[1]})
+            </>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
